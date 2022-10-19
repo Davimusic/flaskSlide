4d9f8fdc131d62fa174126e23f932d475a3984d0
daviMusic <davipianof@gmail.com>
2022-10-19 15:47:56 -0500

    s

--- static/js/ajustadorTamanosContenedores.js
+++ static/js/ajustadorTamanosContenedores.js
@@ -7,25 +7,53 @@ let alturaPantalla = window.innerHeight; //screen.height;
 let anchoPantalla = window.innerWidth; //screen.width;
 
 let anchoPantallaDisponible = (anchoPantalla / 100) * 95; // le restamos 4% de los 2% padding, tambièn le doy un pequeño matgen de error
-let cod = ""
+
 let primeraReferencia = alturaPantalla/3.5
+
+
 
 function ActualizarMostrario2(acc){
 
     contenedor.style.height = `${alturaPantalla/3}px`
     contenedor.style.background = "#008a8a"
-
-    let conteo = 0
+    let cod = ""
+    let puntero = 0
+    let arrePaso = []
 
     cod += 
     `
     <div class="flex espacioEquilatero">
     `
+
     for (let i = primeraReferencia; i < anchoPantallaDisponible && i < (i + primeraReferencia); i++) {
-        cod += `<img class="borde1" style="height: ${alturaPantalla/3.5}px ;" src="../static/images/${arre[conteo]}" alt="">`
-        conteo += 1
+        puntero += 1
         i += alturaPantalla/3.5
     }
+
+    for (let i = 0; i < arre.length; i++) {
+        if(i < puntero ){
+            cod += `<img class="borde1 slide" style="height: ${alturaPantalla/3.5}px ;" src="../static/images/${arre[i]}" alt="">` 
+        } else{
+            cod += `<img class="borde1 slide esconder" style="height: ${alturaPantalla/3.5}px ;" src="../static/images/${arre[i]}" alt="">` 
+        }
+    }
+
+    let pasos = 0
+    while(pasos < arre.length){
+        if(puntero < arre.length){
+            arrePaso.push(arre[puntero])
+        } else {
+            puntero = 0
+            arrePaso.push(arre[puntero])
+        }
+        puntero += 1
+        pasos += 1
+    }
+
+    arre = arrePaso
+    console.log(arre);
+
+    
 
     cod += 
     `
@@ -37,8 +65,8 @@ function ActualizarMostrario2(acc){
     `                                                                                                         
         <div style="height: ${alturaPantalla/3}px; width: ${(anchoPantalla / 100) * (100 - 4)}px; background: #343b4100; position:absolute; top:4%; z-index: 100;" class="">
             <div class="flex espacioEquilatero ">
-                <img style="padding-top: ${alturaPantalla/(3 * 3)}px; height: 50px; width: 50px; padding-right: ${((anchoPantalla / 100) * (100 - 4)) - 100}px;" onclick="ActualizarMostrario('adelante')" src="../static/images/adelante.png" alt="" >
-                <img style="padding-top: ${alturaPantalla/(3 * 3)}px; height: 50px; width: 50px;" onclick="ActualizarMostrario('atras')" src="../static/images/atras.png" alt=""  >
+                <img style="padding-top: ${alturaPantalla/(3 * 3)}px; height: 50px; width: 50px; padding-right: ${((anchoPantalla / 100) * (100 - 4)) - 100}px;" onclick="ActualizarMostrario2('adelante')" src="../static/images/adelante.png" alt="" >
+                <img style="padding-top: ${alturaPantalla/(3 * 3)}px; height: 50px; width: 50px;" onclick="ActualizarMostrario2('atras')" src="../static/images/atras.png" alt=""  >
             </div>
             <div style="padding-top: ${alturaPantalla/(3 * 4)}px; class="flex">
     `
@@ -58,7 +86,7 @@ function ActualizarMostrario2(acc){
 
     `
 
-    console.log(cod);
+    //console.log(cod);
     contenedor.innerHTML = cod;
 }
 
