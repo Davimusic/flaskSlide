71577a810e47e2fbb8775900ab822cffbfd7aea7
daviMusic <davipianof@gmail.com>
2022-10-21 17:20:21 -0500

    21-1

--- static/js/ajustadorTamanosContenedores.js
+++ static/js/ajustadorTamanosContenedores.js
@@ -1,6 +1,6 @@
 //NOTA: las figuras siempren deben ser un cuadrado y de la misma medida.
 
-let arre = [["1-1.jpg", "texto1 <br> texto1 <br> texto1 <br> texto1"], ["2-2.jpg", "texto2 <br> texto1 <br> texto1 <br> texto1"], ["3-3.jpg", "texto3"], ["4-4.jpg", "texto4"], ["5-5.jpg", "texto5"], ["6-6.jpg", "texto6"], ["7-7.jpg", "texto7"], ["8-8.jpg", "texto8"]]
+let arre = [["1-1.jpg", "texto1 <br> texto1 <br> texto1 <br> texto1", "0"], ["2-2.jpg", "texto2 <br> texto1 <br> texto1 <br> texto1", "1"], ["3-3.jpg", "texto3", "2"], ["4-4.jpg", "texto4", "3"], ["5-5.jpg", "texto5", "4"], ["6-6.jpg", "texto6", "5"], ["7-7.jpg", "texto7", "6"], ["8-8.jpg", "texto8", "7"]]
 
 let contenedor =  document.getElementById("contenedor1");
 let alturaPantalla = window.innerHeight; //screen.height;
@@ -12,13 +12,16 @@ let primeraReferencia = alturaPantalla/3.5
 
 
 
-function ActualizarMostrario2(acc){
+function ActualizarMostrario2(){
 
     contenedor.style.height = `${alturaPantalla/3}px`
     contenedor.style.background = "#008a8a"
     let cod = ""
-    let puntero = 0
+    let puntero = 0; 
     let arrePaso = []
+    let resaltarPerlota = arre[0][2]
+
+    
 
     //inicio de creacion de div que se sobrepone para usar flechas y botones                                         // doble del pading establecido
     cod += 
@@ -36,6 +39,8 @@ function ActualizarMostrario2(acc){
             puntero += 1
             i += alturaPantalla/3.5
         }
+
+        
 
         for (let i = 0; i < arre.length; i++) {
             if(i < puntero ){
@@ -69,9 +74,15 @@ function ActualizarMostrario2(acc){
     //calculo la cantidad de imagenes disponibles
 
     for (let i = 0; i < arre.length; i++) {
+        let decision = ""
+        if(resaltarPerlota == i){
+            decision = "circuloRelleno.png"
+        } else {
+            decision = "circuloVacio.png"
+        }
         cod += 
                 `
-                <img style="height: 20px; width: 20px;  padding-right: 10px;  padding-left: 10px;" src="../static/images/circuloVacio.png" alt="" >
+                <img onclick="pelota(${i})" style="height: 20px; width: 20px;  padding-right: 10px;  padding-left: 10px;" src="../static/images/${decision}" alt="" >
                 `
     }
 
@@ -102,11 +113,70 @@ function ActualizarMostrario2(acc){
     }
 
     arre = arrePaso
-    console.log(arre);
+    //console.log(arre);
+}
+
+
+function pelota(id){
+    let empezar = "no"
+    let conteo  = 0;
+    let arrePaso = []
+    for(let i = 0; i < arre.length; i++) {
+        if(id == arre[i][2]){
+            empezar = "si"
+            console.log("empieza " + id);
+            console.log("i " + i);
+        }
+        if(empezar == "si"){
+            arrePaso.push(arre[i])
+            conteo += 1;
+        }
+    }
+
+    for (let i = 0; i < (arre.length - conteo); i++) {
+        arrePaso.push(arre[i])
+    }
+
+    arre = arrePaso;
+    ActualizarMostrario2()
+    console.log(arrePaso);
+    
+
+
 }
 
 
 
 
+/**
+ * console.log(arre);
+    
+    let arrePaso = []
+    for (let i = 0; i < arre.length; i++) {
+        if(arre[i][2]  == id){
+            empezar = "si"
+            console.log(id);
+            console.log("empieza desde el " + i);
+        }
 
+        if(empezar == "si"){
+            arrePaso.push(arre[i])
+            conteo += 1
+        }
+    }
 
+    //console.log(arrePaso);
+    
+
+    if(conteo >= (arre.length - 1)){
+        console.log("acaba ciclo");
+        empezar = "no"
+        console.log(arrePaso);
+        //ActualizarMostrario2()
+        //console.log(arre);
+    } else {
+        console.log("entra pelota");
+        console.log("conteo " + conteo + " arre.length - 1 " + (arre.length - 1));
+        pelota(id)
+    }
+ */
